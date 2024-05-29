caf2fd7df0f34b9297f9ea5b239f287ad250a3cf
Gourav <gouravjangra033@gmail.com>
2024-05-29 23:50:18 +0530

    Updated

--- src/components/DetailedCard.jsx
+++ src/components/DetailedCard.jsx
@@ -12,7 +12,7 @@ function DetailedCard({ display = "hidden" }) {
     console.log(selector)
     return (
         <div className={`${display}`}>
-        <div  className={` rounded-3xl flex items-center justify-center flex-col sm:flex-col lg:flex-row md:flex-row fixed  top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] h-[75vh] sm:h-[75vh] md:h-[50vh] lg:h-[50vh] w-[80vw] overflow-hidden bg-gray-500 border border-zinc-700`}>
+        <div  className={`overflow-y-auto rounded-3xl flex items-center justify-center flex-col sm:flex-col lg:flex-row md:flex-row fixed  top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] h-[75vh] sm:h-[75vh] md:h-[50vh] lg:h-[50vh] w-[80vw] overflow-hidden bg-gray-300 border border-zinc-700`}>
             <img className='sm:w-58 w-48 mix-blend-multiply' src={selector.image} alt="" />
             <div className='flex flex-col gap-6 p-4'>
                 <div><label className='text-blue-500' htmlFor="">Category :</label> {selector.category}</div>
